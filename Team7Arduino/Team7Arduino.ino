08a8421ffa77e985a629d78bee3a9372db297f99
seanriverwmize <39755346+seanriverwmize@users.noreply.github.com>
2021-06-30 16:31:09 -0700

    Add files via upload
    Added main file for use in demonstration

---
+++ Team7Arduino/Team7Arduino.ino
@@ -0,0 +1,77 @@
+#include <Stepper.h>
+
+const int command; // Commands are read throught the Serial port as integers
+int roomLight = 600;  // minimum analog reading from photocell when exposed to room light
+const totalSteps = 200; // Total number of steps per rotation of shutter motor
+
+Stepper stepper(totalSteps, 12, 11, 9, 7); // Team 7 Pins used in order: AIN1, AIN2, BIN2, BIN1
+//Stepper stepper(totalSteps, 7, 9, 10, 11); //FLATSAT PINS order: AIN1, AIN2, BIN2, BIN1
+
+void setup(void) {
+  // Setup function needs to run once when instrument is plugged in
+  Serial.begin(9600);
+  stepper.setSpeed(60); // set the speed of the motor to 30 RPMs   
+}
+
+void captureImageGroup(){ //Placeholder!!!!!!!!!!!!
+  
+}
+
+bool shutterStatus(int minumum) { // "minimum" parameter is minumum analog reading when shutter is open
+  int photocellPin = 1;            // more light on photocell means higher voltage reading on analog pin 0
+  int photocellAnalogReading;     // the analog reading from the voltage divider
+            
+  photocellAnalogReading = analogRead(photocellPin); //analogRead converts Voltage value to an integer 0 - 1023 
+  Serial.print("Photocell Reading: ");  
+  Serial.print(photocellAnalogReading);  // the raw analog reading prints to Serial
+  Serial.println("/1023");  
+  if (photocellAnalogReading >= minumum){
+  Serial.println("Shutter is Open");
+  return true;
+  } else {
+  Serial.println("Shutter is Closed");
+  return false;
+  }
+}
+
+void shutterOpen(int motorSteps){   // parameter = number of 1/200 rotations motor will make if shutter is closed. param should be replaced with concrete value after testing
+  if (shutterStatus() == false)   // If shutter is closed, rotate motor. Otherwise, end function.
+    stepper.step(motorSteps);   // Squiggly brackets not needed with only one statement.
+}
+
+void shutterClose(){
+  while (shutterStatus() == true){
+    stepper.step(1);
+  }
+}
+
+void loop(){  
+  if(Serial.available() > 0){
+    command = Serial.readString();
+    Serial.print("Command: ");
+    Serial.println(command);
+    switch(command){
+      case 1:
+        captureImageGroup();
+        break;
+      case 2:
+        shutterStatus(roomLight);
+        break;
+      case 3:
+        shutterOpen(30); // arbitrary argument. Replace after testing.!!!!!!!!!!!
+        break;
+      case 4:
+        shutterClose();
+        break;
+      case 5:
+        captureImage();
+        break;
+      case 6:
+        showInternalTemp();
+        break;
+      default:
+        Serial.println("INVALID command. Please try again.");
+        break;
+    }
+  }
+}
