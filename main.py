4344587d4d0f97ebebaaf4deb178576a9e0a2d4d
seanriverwmize <39755346+seanriverwmize@users.noreply.github.com>
2021-07-04 21:42:53 -0700

    Update main.py
    Started writing imagegroup logic in python. Considering going back to arduino, so that only the temp arrays with max temps closest to the average max temp will be sent to python. Would probably be less messy.

--- main.py
+++ main.py
@@ -1,25 +1,11 @@
-import serial
-import PySimpleGUI as sg
-import numpy
-import matplotlib.pyplot as plt
-# layout = [
-#     [sg.Text("Hey World"), sg.Text("00")],
-#     [sg.Button("RUN COMMAND"), sg.Button("RUN COMMAND"), sg.Button("RUN COMMAND"), sg.Button("RUN COMMAND"), sg.Button("RUN COMMAND")]
-# ]
-
-# window = sg.Window("Window", layout, margins=(600,400))
-
-# while True:
-#     event, values = window.read()
-#     if event == "RUN COMMAND" or event == sg.WIN_CLOSED:
-#         break
-# window.close()
-
 port = serial.Serial(port = "COM5", baudrate = 9600, timeout = 4) #connect to Arduino Port
 arduino_serial_data = port.readline().decode('utf-8') # Read and Translate Serial data 
 print(arduino_serial_data)
 
 heat_array = numpy.zeros((8, 8))
+imageGroupCount = 15
+newTempCount = 1
+groupTempAverage = 0.0
 
 def make_heatmap():
     heat_values = arduino_message.split(" ")
@@ -45,14 +31,24 @@ def make_heatmap():
     fig.tight_layout()
 while True:
     x = input("What would you like to send: ").encode()
-    port.write(x)
-    arduino_message = port.read_until(expected="&".encode('utf-8')).decode('utf-8')
-    arduino_message = arduino_message[:-1]
-    if x == b'5':
-      arduino_message = arduino_message[:-1]
-      make_heatmap()
-      plt.savefig(fname="C:/Users/smize1/Documents/Heatmap.png", format="png")
-      print(heat_array.max())
+    if x == b'1':
+      port.write(b'3') #shutter opens
+      for i in range(imageGroupCount):
+        port.write(b'5') #captures image
+        arduino_message = port.read_until(expected="&".encode('utf-8')).decode('utf-8')
+        arduino_message = arduino_message[:-2]
+        make_heatmap()
+        groupTempAverage += heat_array.max
+        map_file_name = "C:/Users/smize1/Documents/" + str(heat_array.max) + "Heatmap
+        i += 1
     else:
+      port.write(x)
+      arduino_message = port.read_until(expected="&".encode('utf-8')).decode('utf-8')
+      arduino_message = arduino_message[:-1]
+      if x == b'5':
+        arduino_message = arduino_message[:-1]
+        make_heatmap()
+        plt.savefig(fname="C:/Users/smize1/Documents/Heatmap.png", format="png")
+        print(heat_array.max())
+      else:
         print(arduino_message)
-    
