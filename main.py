a7662845ba18d00d63db0fd0447426f80b78ea61
seanriverwmize <39755346+seanriverwmize@users.noreply.github.com>
2021-07-04 19:11:07 -0700

    Update main.py
    made a function for creating heat maps so it can be used in captureImageGroup

--- main.py
+++ main.py
@@ -19,38 +19,40 @@ port = serial.Serial(port = "COM5", baudrate = 9600, timeout = 4) #connect to Ar
 arduino_serial_data = port.readline().decode('utf-8') # Read and Translate Serial data 
 print(arduino_serial_data)
 
+heat_array = numpy.zeros((8, 8))
+
+def make_heatmap():
+    heat_values = arduino_message.split(" ")
+    for i in range(64):
+      for j in range(8):
+        for z in range(8):
+          heat_array[j][z] = float(heat_values[i])
+          i += 1
+          z += 1
+        z = 0  
+        j += 1
+      j = 0
+      if(i == 64): break;
+    print(heat_array)     
+    fig, ax = plt.subplots()
+    im = ax.imshow(heat_array)
+    cbar = ax.figure.colorbar(im, ax=ax)
+    cbar.ax.set_ylabel("Degrees Celcius", rotation=-90, va="bottom")
+    for i in range(8):
+      for j in range(8):
+        text = ax.text(j, i, heat_array[i, j], ha="center", va="center", color="w")
+    ax.set_title("Thermal Image")
+    fig.tight_layout()
 while True:
     x = input("What would you like to send: ").encode()
     port.write(x)
     arduino_message = port.read_until(expected="&".encode('utf-8')).decode('utf-8')
     arduino_message = arduino_message[:-1]
     if x == b'5':
-        arduino_message = arduino_message[:-1]
-        heat_values = arduino_message.split(" ")
-        #print(len(heat_values))
-        heat_array = numpy.zeros((8, 8))
-        for i in range(64):
-          for j in range(8):
-            for z in range(8):
-              heat_array[j][z] = float(heat_values[i])
-              #print(heat_values[i])
-              i += 1
-              z += 1
-            z = 0  
-            j += 1
-          j = 0
-          if(i == 64): break;
-        print(heat_array)
-          
-        fig, ax = plt.subplots()
-        im = ax.imshow(heat_array)
-        cbar = ax.figure.colorbar(im, ax=ax)
-        cbar.ax.set_ylabel("Degrees Celcius", rotation=-90, va="bottom")
-        for i in range(8):
-          for j in range(8):
-            text = ax.text(j, i, heat_array[i, j], ha="center", va="center", color="w")
-        ax.set_title("Thermal Image")
-        fig.tight_layout()
-        plt.savefig(fname="C:/Users/smize1/Documents/Heatmap.png", format="png")
-        plt.show()
+      arduino_message = arduino_message[:-1]
+      make_heatmap()
+      plt.savefig(fname="C:/Users/smize1/Documents/Heatmap.png", format="png")
+      print(heat_array.max())
+    else:
+        print(arduino_message)
     
