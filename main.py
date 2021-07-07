32c6b2d631d5171a28f297d2495b5d452cf50aaf
seanriverwmize <39755346+seanriverwmize@users.noreply.github.com>
2021-07-06 23:04:12 -0700

    Update main.py
    Image group take 45 and returns 15 images. A unique folder is created for each image group

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import serial
 import time
 import numpy
+from os import mkdir
 import matplotlib.pyplot as plt
 
 port = serial.Serial()
 port.baudrate = 9600
-port.port = "COM6"
-port.timeout = 10
-#port = serial.Serial(port = "COM6", baudrate = 9600, timeout = 10) #connect to Arduino Port
+port.port = "COM5"
+port.timeout = 25
 
-imageGroupCount = 15
+imageGroupCount = 45
 newTempTag = 1
 groupTempAverage = 0.0
 
@@ -27,9 +27,11 @@ def make_heatmap(array):
     ax.set_title("Thermal Image")
     fig.tight_layout()
     return heat_array
-
+def make_folder(folder_path):
+  mkdir(folder_path)
 def find_max_difference(x):
     return abs(x.max() - group_temp_average)
+
 while True:
     x = input("What would you like to send: ").encode()
     port.open()
@@ -38,24 +40,27 @@ while True:
     port.close()
     arduino_message = arduino_message[:-1]
     if x == b'1':
+      current_time = time.strftime("%H-%M-%S", time.localtime())
       arduino_message = arduino_message[:-1]
       heat_array_group = arduino_message.split(" ")
       for i in range(len(heat_array_group)):
         heat_array_group[i] = float(heat_array_group[i])
-      heat_array_group = numpy.reshape(heat_array_group, (15, 64))
+      heat_array_group = numpy.reshape(heat_array_group, (imageGroupCount, 64))
       group_temp_average = 0.0
-      for i in range(15):
+      for i in range(imageGroupCount):
         group_temp_average += heat_array_group[i].max()
         #print(heat_array_group[i])
-      group_temp_average /= 15
+      group_temp_average /= imageGroupCount
+      group_folder = "C:/Users/smize1/Documents/({}) {:.2f}".format(current_time, group_temp_average)# + current_time + ") " + str(group_temp_average)
+      make_folder(group_folder)
       #print(group_temp_average)
       heat_array_group = sorted(heat_array_group, key=find_max_difference)
-      heat_array_group = heat_array_group[:5]
+      heat_array_group = heat_array_group[:15]
       #for i in range(len(heat_array_group)):
         #print(heat_array_group[i].max())
       for i in range(len(heat_array_group)):
         make_heatmap(heat_array_group[i])
-        plt.savefig(fname="C:/Users/smize1/Documents/" + str(group_temp_average) + "/Heatmap" + str(i+1) + "(" + str(heat_array_group[i].max()) + ").png", format="png")
+        plt.savefig(fname= group_folder + "/map" + str(i+1) + "(" + str(heat_array_group[i].max()) + ").png", format="png")
     elif x == b'5':
       arduino_message = arduino_message[:-1]
       heat_array_single = arduino_message.split(" ")
