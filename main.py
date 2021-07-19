63486bdc7ddd72529841773606a37797d8c8b8c6
seanriverwmize <39755346+seanriverwmize@users.noreply.github.com>
2021-07-19 10:59:09 -0500

    Update main.py
    list constructors

--- main.py
+++ main.py
@@ -38,11 +38,11 @@ while True:
     arduino_message = arduino_message[:-1]
     if x == b'1':
       current_time = time.strftime("%H-%M-%S", time.localtime())
-      arduino_message = arduino_message[:-1]
-      heat_array_group = arduino_message.split(" ")
+#       arduino_message = arduino_message[:-1]
+#       heat_array_group = arduino_message.split(" ")
 #       for i in range(len(heat_array_group)):
 #         heat_array_group[i] = float(heat_array_group[i])
-      heat_array_group = [float(i) for i in heat_array_group] #list constructor version
+      heat_array_group = [float(i) for i in arduino_message.split(" ")[:-1]] #list constructor version
       heat_array_group = numpy.reshape(heat_array_group, (imageGroupCount, 64))
       group_temp_average = 0.0
       for i in range(imageGroupCount):
@@ -52,18 +52,19 @@ while True:
       group_folder = "C:/Users/smize1/Documents/({}) {:.2f}".format(current_time, group_temp_average)# + current_time + ") " + str(group_temp_average)
       mkdir(group_folder)
       #print(group_temp_average)
-      heat_array_group = sorted(heat_array_group, key=find_max_difference)
-      heat_array_group = heat_array_group[:15]
+      heat_array_group = sorted(heat_array_group, key=find_max_difference)[:15]
+      #heat_array_group = heat_array_group[:15]
       #for i in range(len(heat_array_group)):
         #print(heat_array_group[i].max())
-      for i in range(len(heat_array_group)):
+      for i in heat_array_group:
         make_heatmap(heat_array_group[i])
         plt.savefig(fname= group_folder + "/map" + str(i+1) + "(" + str(heat_array_group[i].max()) + ").png", format="png")
     elif x == b'5':
       arduino_message = arduino_message[:-1]
-      heat_array_single = arduino_message.split(" ")
-      for i in range(64):
-        heat_array_single[i] = float(heat_array_single[i])
+#       heat_array_single = arduino_message.split(" ")[:-1]
+#       for i in range(64):
+#         heat_array_single[i] = float(heat_array_single[i])
+      heat_array_single = [float(i) for i in arduino_message.split(" ")[:-1]]
       heat_array = make_heatmap(heat_array_single)
       #print(heat_array.max())
       plt.savefig(fname="C:/Users/smize1/Documents/Heatmap(" + str(heat_array.max()) + ").png", format="png") 
